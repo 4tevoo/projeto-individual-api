109c0143f8a37b35d46f16ebc815830734a4d883
Tevo <estevao.cunha@residente.serratec.org>
2026-05-14 02:45:02 -0300

    Subindo a entity Veiculo mais uma vez de madrugada.... eu preciso ajeitar meu horário, prometo que o que eu fizer amanhã eu subo de dia. Coloca os comentários do código na culpa do sono prof. fui, boa noite, posso ausentar a caçada de fantasmas pq de entidades já acabou, Veiculo e Cliente se eu nao me engano

---
+++ src/main/java/org/serratec/concessionaria/entity/Veiculo.java
@@ -0,0 +1,52 @@
+package org.serratec.concessionaria.entity;
+
+// Imports necessários
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import java.util.UUID;
+
+@Entity // esse faz o banco saber que é uma tabela
+@Data // pro lombok trabalhar pra nós pq programador quer evitar o máximo de esforço sempre
+@AllArgsConstructor // construtor tudão
+@NoArgsConstructor // construtor nadinha
+public class Veiculo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotBlank(message = "A marca é obrigatória")
+    private String marca;
+
+    @NotBlank(message = "O modelo é obrigatório")
+    private String modelo;
+
+    @NotNull
+    @Min(1900) // não vamo ta pegando carro TÃO antigo assim né
+    private Integer ano;
+
+    @NotNull
+    @DecimalMin("1.0") // vender carro a preço de banana tudo bem mas de graça não né
+    private Double valor;
+
+    @NotBlank
+    @Column(unique = true) // nada de clonagem de placa aqui, se não DETRAN te pega
+    private String placa;
+
+    @NotNull
+    private Double maximoDesconto;
+
+    @NotNull
+    private Boolean vendido;
+
+    // seguindo as regrinhas do Swagger do prof Cadu, trato depois no service
+    private Double valorVenda;
+
+    // muitos carros podem ter um dono muito rico, aquele cliente que a loja gosta
+    @ManyToOne
+    @JoinColumn(name = "cliente_id", nullable = false)
+    private Cliente cliente;
+}
